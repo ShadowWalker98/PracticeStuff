41df338d4cae45e17df86be6234f23fdcf8e34a3
ShadowWalker98 <44528434+ShadowWalker98@users.noreply.github.com>
2020-06-22 15:00:26 +0530

    objects

---
+++ app.js
@@ -0,0 +1,12 @@
+function getStats(a, b) {
+    let resultAdd = a + b;
+    let resultDiff = a - b;
+
+    return {
+        sum: resultAdd;
+        difference: resultDiff;
+    };
+}
+
+let stats = getStats(4,2);
+console.log(stats);
